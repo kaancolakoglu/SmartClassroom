7d7f6245db6e296987c9149f0e355f03da39bb15
Kaan Çolakoğlu <kaancolakoglu3@gmail.com>
2025-03-30 01:01:52 +0300

    First commit

---
+++ src/test/java/com/smartclassroom/occupancysystem/OccupancySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartclassroom.occupancysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OccupancySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
